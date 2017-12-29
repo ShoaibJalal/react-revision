29e11277945d049b5f1de30cdf690216e1bc686c
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-29 12:10:46 +0200

    adding button and event handler

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log("Button is clicked.");
+  }
+
   render() {
     return (
       <div className="App">
        <h1>I am React App.</h1>
+       <button onClick= {this.switchNameHandler}>Switch name</button>
        <Person name = {this.state.persons[0].name} age={this.state.persons[0].age}>Hobbies: Programming </Person>
        <Person name ={this.state.persons[1].name} age = {this.state.persons[1].age} />
        <Person name ={this.state.persons[2].name} age ={this.state.persons[2].age}/>
