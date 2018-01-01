e7f273060dfcf6657c797da7bcdd99b76cc60462
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2018-01-01 11:02:28 +0200

    Adding inline styles

--- src/App.js
+++ src/App.js
@@ -34,10 +34,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
        <h1>I am React App.</h1>
-       <button onClick= {this.switchNameHandler.bind(this, 'Shoaib!')}>Switch name</button>
+       <button
+       style = {style} 
+       onClick= {this.switchNameHandler.bind(this, 'Shoaib!')}>Switch name</button>
        <Person 
        name = {this.state.persons[0].name}
        age={this.state.persons[0].age}
