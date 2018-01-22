253010d097be1ceaeaa95618aa353e5bb9af380c
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2018-01-22 11:57:08 +0200

    outputing component in lists

--- src/App.js
+++ src/App.js
@@ -50,6 +50,19 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (<div>
+        {this.state.persons.map(person => {
+          return <Person 
+          name = {person.name}
+          age = {person.age}/>
+        })}
+     
+      </div>)
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -57,21 +70,8 @@ class App extends Component {
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { this.state.showPersons === true ?
-          <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age} />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Shaun!')}
-            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age} />
-          </div> : null
-        }
+        
+        {persons}
         
       </div>
     );
