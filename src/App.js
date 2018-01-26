d1299deee50b7bdab3e5eb06377f4446dc9d92e2
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2018-01-26 12:21:59 +0200

    Removing Radium from project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -51,9 +50,7 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'darkred'
-      }
+      
     };
 
     let persons = null;
@@ -72,9 +69,7 @@ class App extends Component {
      
       </div>)
        style.backgroundColor = '#FF7F50'; /*coral*/
-      style[':hover'] = {
-        backgroundColor: 'chocolate'
-      }
+      
     }
     let classes = [];
     if(this.state.persons.length <= 2){
@@ -85,7 +80,7 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
+     
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className = {classes.join(' ')}>This is really working!</p>
@@ -96,10 +91,10 @@ class App extends Component {
         {persons}
         
       </div>
-      </StyleRoot>
+      
     );
     
   }
 }
 
-export default Radium(App);
+export default App;
