5798a9fe00b63bf0b8a18809cd075149ae75e275
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2018-01-30 14:40:17 +0200

    Splitting the app into components and making app.js leaner

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+
+    let implementedClasses = [];
+    let btnClass = '';
+    if(props.showPersons){
+        btnClass = classes.Green;
+    }
+    
+    if(props.persons.length <= 2){
+      implementedClasses.push(classes.Danger); // classes = ['Danger']
+    }
+    if (props.persons.length <= 1){
+      implementedClasses.push(classes.Bold); // classes = ['Danger', 'Bold']
+    }
+
+
+    return(
+        <div className={classes.Cockpit}>
+        <h1>Hi, I'm a React App</h1>
+        <p className = {implementedClasses.join(' ')}>This is really working!</p>
+        <button
+          className = {btnClass} 
+          onClick={props.clicked}>Toggle Persons</button>
+         </div> 
+    );
+}
+export default cockpit;
